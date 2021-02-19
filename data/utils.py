05105ece65eb29675627c4c16610447810d7bdf1
kesamet <kesamet@gmail.com>
2021-02-19 14:25:12 +0800

    Update data utils

--- data/utils.py
+++ data/utils.py
@@ -4,9 +4,6 @@ import pandas as pd
 import streamlit as st
 from sklearn import metrics
 
-from xai_fairness.toolkit_xai import compute_shap, get_explainer, compute_corrcoef
-
-
 @st.cache(allow_output_mutation=True)
 def load_model(filename):
     return pickle.load(open(filename, "rb"))
@@ -23,12 +20,6 @@ def load_data(filename, sample_size=None, random_state=0):
 @st.cache(allow_output_mutation=True)
 def predict(clf, x):
     return clf.predict_proba(x)[:, 1]
-
-
-@st.cache(allow_output_mutation=True)
-def compute_shap_values(clf, x):
-    explainer = get_explainer(model=clf, model_type="tree")
-    return compute_shap(explainer, x)
 
 
 @st.cache
