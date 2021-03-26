8f360baf57ae503705d78b5d15ff75559f9a55a7
Kok-Meng Tan <kesamet@gmail.com>
2021-03-26 10:30:05 +0800

    Fix serve_http

--- serve_http.py
+++ serve_http.py
@@ -19,8 +19,8 @@ redis = pd.read_parquet("/artefact/test.gz.parquet")
 def read_redis_features(sk_id):
     """Gets all the values from redis."""
     # Simulate querying redis
-    row = redis.query(f"SK_ID_CURR == '{sk_id}'")
-    if len(row) == 0:
+    row = redis.query(f"SK_ID_CURR == {sk_id}")
+    if row.empty:
         return None
     return row[feature_cols]
 
